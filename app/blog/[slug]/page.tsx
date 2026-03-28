122b59f92ca3a839e44b4b2b1701a6feec6f6fe7
Oliver Petrick <odpetrick@berkeley.edu>
2026-03-28 11:26:38 -0700

    generateStaticRoutes now reads the markdown posts directory for static routes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path';
+
 export default async function Page({
   params,
 }: {
@@ -7,10 +10,14 @@ export default async function Page({
   const { default: Post } = await import(`@/app/markdown/blog-posts/${slug}.mdx`)
  
   return <Post />
-}
+};
  
+/**
+ * Reads the markdown posts and adds them to the static params to create static routes. 
+ */
 export function generateStaticParams() {
-  return [{ slug: 'firesim' }]
-}
+  const files = fs.readdirSync(path.join(process.cwd(), 'app/markdown/blog-posts'))
+  return files.map(f => ({ slug: f.replace('.mdx', '') }))
+};
  
-export const dynamicParams = false
+export const dynamicParams = false;
