d7e65ba8f0a92a8de9264053d9b85faf34b7be9e
Oliver Petrick <odpetrick@berkeley.edu>
2026-03-29 15:50:41 -0700

    Added styling to the heading

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -19,13 +19,18 @@ export default async function Page({
   return (
     <div className="flex gap-8">
       <div className="w-3/4">
-        <h1>
-          {metadata.title}
-        </h1>
-        <h2>
-          {metadata.date}
-        </h2>
-        <Post />
+        <div className="my-8">
+          <h1 className="text-3xl font-bold">
+            {metadata.title}
+          </h1>
+          <h2 className="text-sm text-gray-400">
+            {metadata.date}
+          </h2>
+          <hr className="my-2 border-gray-200" />
+        </div>
+        <article className="prose">
+          <Post />
+        </article>
       </div>
       <div className="w-1/4 shrink-0 sticky top-6 self-start">
         <Toc headings={headings} />
